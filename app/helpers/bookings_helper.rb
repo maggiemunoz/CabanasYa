103a08241d3b101402707f24092e56153bc91bb1
Magdalena Muñoz <maggie.munoz@uc.cl>
2022-02-06 14:52:16 -0300

    validators

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 module BookingsHelper
-  def nearest_date(cabin, bookings)
+  def nearest_date(cabin)
     date = Date.today
-    
+    date += 1.days while taken?(date, cabin)
     date
   end
 
-  def taken?(date, _cabin, bookings)
-    puts date
-    bookings.each do |booking|
-      return true if (date >= booking.start_date) && (date <= booking.end_date)
+  def taken?(date, _cabin)
+    Booking.all.each do |booking|
+      return true if (date >= booking.start_date.to_date) & (date <= booking.end_date.to_date)
     end
     false
   end
