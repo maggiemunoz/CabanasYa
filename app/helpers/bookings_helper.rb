1e01472de3b88ed15383bf710140e45d4fec740a
Magdalena Muñoz <maggie.munoz@uc.cl>
2022-02-06 01:35:09 -0300

    created helpers

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module BookingsHelper
+  def nearest_date(cabin, bookings)
+    date = Date.today
+    
+    date
+  end
+
+  def taken?(date, _cabin, bookings)
+    puts date
+    bookings.each do |booking|
+      return true if (date >= booking.start_date) && (date <= booking.end_date)
+    end
+    false
+  end
 end
