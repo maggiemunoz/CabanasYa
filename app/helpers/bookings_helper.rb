2c900b36f5a7291ba9f247f34b101f4e666f55fe
Maggie Muñoz <62019819+maggiemunoz@users.noreply.github.com>
2022-02-06 14:54:41 -0300

    Add nearest date info in views (#4)
    * created helpers
    
    * validators

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module BookingsHelper
+  def nearest_date(cabin)
+    date = Date.today
+    date += 1.days while taken?(date, cabin)
+    date
+  end
+
+  def taken?(date, _cabin)
+    Booking.all.each do |booking|
+      return true if (date >= booking.start_date.to_date) & (date <= booking.end_date.to_date)
+    end
+    false
+  end
 end
