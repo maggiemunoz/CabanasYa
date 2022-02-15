8c73b935e1326848169e97fa2156ae6feb5cf949
Maggie Muñoz <62019819+maggiemunoz@users.noreply.github.com>
2022-02-15 13:37:13 -0300

    Feat/better testing (#17)
    * booking tests
    
    * contact tests
    
    * fix rubocop

--- spec/request/contact_request_spec.rb
+++ spec/request/contact_request_spec.rb
@@ -2,9 +2,9 @@
 
 require 'rails_helper'
 
-class BookingTest < ActiveSupport::TestCase
-  RSpec.describe 'Bookings', type: :request do
-    describe 'book cabin' do
+class ContactTest < ActiveSupport::TestCase
+  RSpec.describe 'Contacts', type: :request do
+    describe 'send contact information' do
       before do
         cabin = Cabin.create!(name: 'Cabaña Test', description: 'Es una cabaña de prueba', price: 10_000)
         user = User.create!(email: 'cabanasyacorreo@gmail.com', password: ENV['DEFAUL_USER_PASS'])
@@ -22,7 +22,7 @@ class BookingTest < ActiveSupport::TestCase
         User.delete_all
       end
 
-      it 'book cabin correctly' do
+      it 'fail if rut missing' do
         expect do
           visit root_path
           find('#cabin').click
@@ -33,78 +33,64 @@ class BookingTest < ActiveSupport::TestCase
           # sleep(1)
           click_on 'Agendar'
           # sleep(1)
-          expect(page).to have_content('La reserva fue creada correctamente')
           # find_field('Rut').set('21643640-7')
-          find('#rutfield', visible: false).fill_in(with: '21643640-7')
-          # sleep(3)
+          #   find('#rutfield', visible: false).fill_in(with: '21643640-7')
+          # sleep(1)
           find('#infofield', visible: false).fill_in(with: 'testing')
           # fill_in 'Informacion adicional', with: 'testing'
-          # sleep(3)
+          # sleep(1)
           click_on 'Enviar'
 
+          expect(page).to have_content("Rut can't be blank")
+        end.not_to raise_error
+      end
+
+      it 'fail if rut is invalid' do
+        expect do
+          visit root_path
+          find('#cabin').click
+          fill_in 'Name', with: 'Test'
+          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
+          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
+          fill_in 'Contact email', with: 'test@gmail.com'
+          # sleep(1)
+          click_on 'Agendar'
+          # sleep(1)
+          # find_field('Rut').set('21643640-7')
+          find('#rutfield', visible: false).fill_in(with: '21643640')
+          # sleep(1)
+          find('#infofield', visible: false).fill_in(with: 'testing')
+          # fill_in 'Informacion adicional', with: 'testing'
+          # sleep(1)
+          click_on 'Enviar'
+
+          expect(page).to have_content('Rut is not a valid rut')
+        end.not_to raise_error
+      end
+
+      it 'fail because no info and then send' do
+        expect do
+          visit root_path
+          find('#cabin').click
+          fill_in 'Name', with: 'Test'
+          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
+          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
+          fill_in 'Contact email', with: 'test@gmail.com'
+          # sleep(1)
+          click_on 'Agendar'
+          # sleep(1)
+          # find_field('Rut').set('21643640-7')
+          find('#rutfield', visible: false).fill_in(with: '21643640-7')
+          # sleep(1)
+          # fill_in 'Informacion adicional', with: 'testing'
+          # sleep(1)
+          click_on 'Enviar'
+          # sleep(1)
+          expect(page).to have_content("Info can't be blank")
+
+          find('#infofield', visible: false).fill_in(with: 'testing')
+          click_on 'Enviar'
           expect(page).to have_content('La informacion de contacto ha sido enviada correctamente.')
-        end.not_to raise_error
-      end
-
-      it 'fail if name missing' do
-        expect do
-          visit root_path
-          find('#cabin').click
-          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
-          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
-          fill_in 'Contact email', with: 'test@gmail.com'
-          # # sleep(1)
-          click_on 'Agendar'
-          # # sleep(1)
-          expect(page).to have_content("Name can't be blank")
-        end.not_to raise_error
-      end
-
-      it 'fail if email missing' do
-        expect do
-          visit root_path
-          find('#cabin').click
-          fill_in 'Name', with: 'Test'
-          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
-          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
-          # # sleep(1)
-          click_on 'Agendar'
-          # # sleep(1)
-          expect(page).to have_content("Contact email can't be blank")
-        end.not_to raise_error
-      end
-
-      it 'fail if dates missing' do
-        expect do
-          visit root_path
-          find('#cabin').click
-          fill_in 'Name', with: 'Test'
-          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
-          fill_in 'Contact email', with: 'test@gmail.com'
-          # # sleep(1)
-          click_on 'Agendar'
-          # # sleep(1)
-          expect(page).to have_content("Start date can't be blank")
-          expect(page).to have_content("End date can't be blank")
-        end.not_to raise_error
-      end
-
-      it 'fail and then book correctly' do
-        expect do
-          visit root_path
-          find('#cabin').click
-          # fill_in 'Fechas', with: '14/07/2022 to 18/07/2022'
-          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
-          fill_in 'Contact email', with: 'test@gmail.com'
-          # sleep(1)
-          click_on 'Agendar'
-          # sleep(1)
-          fill_in 'Name', with: 'Test'
-          find('#flatpickr-capybara-selector-dates').fill_in(with: '14/07/2022 to 18/07/2022')
-          # sleep(3)
-          click_on 'Agendar'
-          # sleep(3)
-          expect(page).to have_content('La reserva fue creada correctamente')
         end.not_to raise_error
       end
     end
